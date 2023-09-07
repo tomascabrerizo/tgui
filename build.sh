1110da039db54108c869713f9c64406601a2e16d
tomascabrerizo <tomascabrerizo97@gmail.com>
2023-09-07 17:45:02 -0300

    clean build script

--- build.sh
+++ build.sh
@@ -6,5 +6,6 @@ then
 fi
 
 clang -pedantic -D_GNU_SOURCE -Wall -Wextra -Werror -std=c99 -g -I./thirdparty -I./code ./thirdparty/stb_truetype.c \
-    ./code/main.c ./code/tgui.c ./code/tgui_memory.c ./code/tgui_gfx.c ./code/tgui_os.c ./code/tgui_painter.c ./code/tgui_geometry.c ./code/tgui_docker.c ./code/tgui_serializer.c \
+    ./code/main.c ./code/tgui.c ./code/tgui_memory.c ./code/tgui_gfx.c ./code/tgui_os.c \
+    ./code/tgui_painter.c ./code/tgui_geometry.c ./code/tgui_docker.c ./code/tgui_serializer.c \
     -o ./build/app -lm -lX11 -lGL -lXcursor -Wno-implicit-fallthrough
